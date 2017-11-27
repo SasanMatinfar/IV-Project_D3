8e77883fc28c966789d7ed3a2704b71afc008f63
SasanMatinfar <sasanmatinfar@gmail.com>
2017-11-27 20:58:42 +0100

    First commit

---
+++ d3js_project/year.js
@@ -0,0 +1,150 @@
+var height = 500;
+var width = 600;
+var margin = {top: 20, right:20, bottom: 50, left: 20};
+	
+var svg = d3.select("body")
+			.append("svg")
+			.attr("width", width + margin.left + margin.right)
+			.attr("height", height + margin.top + margin.bottom)
+			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+	svg.append("g")
+	    .attr("class", "y axis");
+		
+	svg.append("g")
+		.attr("class", "x axis");
+
+var xScale = d3.scale.linear()
+		.rangeRoundBands([margin.left, width], .1);
+		
+var yScale = d3.scale.linear()
+		.range([height, 0]);
+	
+var xAxis = d3.svg.axis()
+		.scale(xScale)
+		.orient("bottom");
+		
+var yAxis = d3.svg.axis()
+		.scale(yScale)
+		.orient("left");
+
+
+
+	d3.csv("year.csv")
+		.row(function (d) { return { anno:Number(d.anno) , perstype:String(d.perstype), unempl:Number(d.unempl), popul:Number(d.popul)};})
+		.get(function(error, data){ console.log(data)})
+		.data(function(data) {
+		
+		// extract the x labels (years) for the axis and scale domain
+		var xLabels = data.map(function (d) { return d.anno; })
+	
+		xScale.domain(xLabels);
+		yScale.domain([0, d3.max(data, function(d) { return parseFloat(d.unempl); })]);
+		
+		var line = d3.svg.line()
+			.x(function(d) { return xScale(d.anno); })
+			.y(function(d) { return yScale(d.unempl); });
+		
+		svg.append("path")
+			.datum(data)
+			.attr("class","line")
+			.attr("d", line);
+		
+		svg.select(".x.axis")
+			.attr("transform", "translate(0," + (height) + ")")
+			.call(xAxis.tickValues(xLabels.filter(function(d, i) { 
+				if (i % 12 == 0)
+					return d;
+				})))
+			.selectAll("text")
+			.style("text-anchor","end")
+			.attr("transform", function(d) {
+				return "rotate(-45)";
+			});
+		
+		svg.select(".y.axis")
+			.attr("transform", "translate(" + (margin.left) + ",0)")
+			.call(yAxis.tickFormat(currencyFormat));
+			
+		// chart title
+		svg.append("text")
+			.attr("x", (width + (margin.left + margin.right) )/ 2)
+			.attr("y", 0 + margin.top)
+			.attr("text-anchor", "middle")
+			.style("font-size", "16px")
+			.style("font-family", "sans-serif")
+			.text("Unemployment Rate in Munich 2000-2015");
+		
+		// x axis label
+		svg.append("text")
+			.attr("x", (width + (margin.left + margin.right) )/ 2)
+			.attr("y", height + margin.bottom)
+			.attr("class", "text-label")
+			.attr("text-anchor", "middle")
+			.text("Years from 2000-2015");
+		
+		// get the x and y values for least squares
+		var xSeries = d3.range(1, xLabels.length + 1);
+		var ySeries = data.map(function(d) { return parseFloat(d.unempl); });
+		
+		var leastSquaresCoeff = leastSquares(xSeries, ySeries);
+		
+		// apply the reults of the least squares regression
+		var x1 = xLabels[0];
+		var y1 = leastSquaresCoeff[0] + leastSquaresCoeff[1];
+		var x2 = xLabels[xLabels.length - 1];
+		var y2 = leastSquaresCoeff[0] * xSeries.length + leastSquaresCoeff[1];
+		var trendData = [[x1,y1,x2,y2]];
+		
+		var trendline = svg.selectAll(".trendline")
+			.data(trendData);
+			
+		trendline.enter()
+			.append("line")
+			.attr("class", "trendline")
+			.attr("x1", function(d) { return xScale(d[0]); })
+			.attr("y1", function(d) { return yScale(d[1]); })
+			.attr("x2", function(d) { return xScale(d[2]); })
+			.attr("y2", function(d) { return yScale(d[3]); })
+			.attr("stroke", "black")
+			.attr("stroke-width", 1);
+		
+		// display equation on the chart
+		svg.append("text")
+			.text("eq: " + decimalFormat(leastSquaresCoeff[0]) + "x + " + 
+				decimalFormat(leastSquaresCoeff[1]))
+			.attr("class", "text-label")
+			.attr("x", function(d) {return xScale(x2) - 60;})
+			.attr("y", function(d) {return yScale(y2) - 30;});
+		
+		// display r-square on the chart
+		svg.append("text")
+			.text("r-sq: " + decimalFormat(leastSquaresCoeff[2]))
+			.attr("class", "text-label")
+			.attr("x", function(d) {return xScale(x2) - 60;})
+			.attr("y", function(d) {return yScale(y2) - 10;});
+	})
+
+	// returns slope, intercept and r-square of the line
+	function leastSquares(xSeries, ySeries) {
+		var reduceSumFunc = function(prev, cur) { return prev + cur; };
+		
+		var xBar = xSeries.reduce(reduceSumFunc) * 1.0 / xSeries.length;
+		var yBar = ySeries.reduce(reduceSumFunc) * 1.0 / ySeries.length;
+		var ssXX = xSeries.map(function(d) { return Math.pow(d - xBar, 2); })
+			.reduce(reduceSumFunc);
+		
+		var ssYY = ySeries.map(function(d) { return Math.pow(d - yBar, 2); })
+			.reduce(reduceSumFunc);
+			
+		var ssXY = xSeries.map(function(d, i) { return (d - xBar) * (ySeries[i] - yBar); })
+			.reduce(reduceSumFunc);
+			
+		var slope = ssXY / ssXX;
+		var intercept = yBar - (xBar * slope);
+		var rSquare = Math.pow(ssXY, 2) / (ssXX * ssYY);
+		
+		return [slope, intercept, rSquare];
+	}
+
